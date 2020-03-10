5d6f996f3ed95dac09c9682568bffb9100c098eb
Raymond <raymond.vanvuuren@true.nl>
2020-03-10 11:05:10 +0100

    add README.md

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace LemonCMS\LaravelCrud;
 
+use LemonCMS\LaravelCrud\Commands\CrudController;
+use LemonCMS\LaravelCrud\Commands\CrudEvents;
 use LemonCMS\LaravelCrud\Commands\CrudGenerator;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
@@ -10,14 +12,17 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $this->publishes([
             __DIR__.'config/crud.php' => config_path('crud.php'),
+            __DIR__.'resources/views' => base_path('resources/views/lemoncms/laravelcrud'),
         ]);
 
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'laravelCrud');
-
         if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+            $this->loadViewsFrom(__DIR__.'/resources/views', 'laravelCrud');
+
             $this->commands([
                 CrudGenerator::class,
+                CrudEvents::class,
+                CrudController::class,
             ]);
         }
     }
